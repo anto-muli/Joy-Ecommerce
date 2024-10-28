3fdd4230a27f684f98d4752448407ce6b5eca96e
anto-muli <antonymusau50@gmail.com>
2024-10-28 20:31:09 +0300

    Done

--- admin/medusa-config.js
+++ admin/medusa-config.js
@@ -86,3 +86,12 @@ module.exports = {
   plugins,
   modules,
 };
+
+module.exports = defineConfig({
+  // ...
+  admin: {
+    disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
+    backendUrl: process.env.MEDUSA_BACKEND_URL,
+    path: process.env.MEDUSA_ADMIN_PATH,
+  },
+})
